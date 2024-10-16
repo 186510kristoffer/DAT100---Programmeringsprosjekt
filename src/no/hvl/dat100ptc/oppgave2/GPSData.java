9540878a811084ebd0c4453f639d85e3fbcbc3f5
186510kristoffer <krissalbrigtsen@gmail.com>
2024-10-16 14:47:59 +0200

    opg 2

--- src/no/hvl/dat100ptc/oppgave2/GPSData.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSData.java
@@ -9,40 +9,40 @@ public class GPSData {
 	protected int antall = 0;
 
 	public GPSData(int antall) {
-
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO
+	    gpspoints = new GPSPoint[antall];  // Initialiserer arrayet
+	    this.antall = 0;  // Setter antall til 0
 	}
+
 
 	public GPSPoint[] getGPSPoints() {
-		return this.gpspoints;
+	    return this.gpspoints;  // Returnerer hele arrayet av GPS-punkter
 	}
-	
-	protected boolean insertGPS(GPSPoint gpspoint) {
 
-		boolean inserted = false;
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
-	
+	protected boolean insertGPS(GPSPoint gpspoint) {
+	    if (antall < gpspoints.length) {  // Sjekker om det er plass i arrayet
+	        gpspoints[antall] = gpspoint;  // Legger til GPS-punktet i arrayet
+	        antall++;  // Øker antall punkter som er lagret
+	        return true;  // Returnerer true når innsettingen er vellykket
+	    }
+	    return false;  // Returnerer false hvis arrayet er fullt
 	}
+
 
 	public boolean insert(String time, String latitude, String longitude, String elevation) {
-
-		GPSPoint gpspoint;
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
-		
+	    // Bruker GPSDataConverter til å konvertere verdiene til riktige datatyper
+	    GPSPoint gpspoint = GPSDataConverter.convert(time, latitude, longitude, elevation);
+	    
+	    // Setter inn det nye GPS-punktet ved hjelp av insertGPS() og returnerer resultatet
+	    return insertGPS(gpspoint);
 	}
+
 
 	public void print() {
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+	    System.out.println("====== Konvertert GPS Data - START ======");
+	    for (int i = 0; i < antall; i++) {
+	        System.out.println(gpspoints[i].toString());  // Skriver ut hvert GPS-punkt
+	    }
+	    System.out.println("====== Konvertert GPS Data - SLUTT ======");
 	}
+
 }
--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -1,30 +1,26 @@
 package no.hvl.dat100ptc.oppgave2;
 
-import no.hvl.dat100ptc.TODO;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class GPSDataConverter {
 
-	
-	private static int TIME_STARTINDEX = 11; 
+    private static int TIME_STARTINDEX = 11;
 
-	public static int toSeconds(String timestr) {
-		
-		int secs;
-		int hr, min, sec;
-		
-		// TODO
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
+    // Metode som konverterer tid fra strengen til sekunder siden midnatt
+    public static int toSeconds(String timestr) {
+        int hr = Integer.parseInt(timestr.substring(TIME_STARTINDEX, 13));
+        int min = Integer.parseInt(timestr.substring(14, 16));
+        int sec = Integer.parseInt(timestr.substring(17, 19));
+        return hr * 3600 + min * 60 + sec;  // Regner ut antall sekunder
+    }
 
-	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
-
-		GPSPoint gpspoint;
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-	
+    // Metode som konverterer strenger til et GPSPoint objekt
+    public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
+        int time = toSeconds(timeStr);  // Konverterer tid til sekunder
+        double latitude = Double.parseDouble(latitudeStr);  // Konverterer breddegrad til double
+        double longitude = Double.parseDouble(longitudeStr);  // Konverterer lengdegrad til double
+        double elevation = Double.parseDouble(elevationStr);  // Konverterer høyde til double
+        return new GPSPoint(time, latitude, longitude, elevation);  // Lager nytt GPSPoint
+    }
 }
+
